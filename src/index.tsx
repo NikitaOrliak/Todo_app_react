7df32f373b0717314a79ec287565026add6181d3
Nikita Orliak <nikitonorl@gmail.com>
2023-12-30 13:04:22 +0100

    clone todo app

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
+import { createRoot } from 'react-dom/client';
+
+import 'bulma/css/bulma.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import './styles/index.scss';
+
 import { App } from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+createRoot(document.getElementById('root') as HTMLDivElement)
+  .render(<App /> as React.ReactNode);
---
+++ src/types/ErrorMessageEnum.ts
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-shadow
+export enum ErrorMessageEnum {
+  TodoLoadError = 'Unable to load todos',
+  TitleError = 'Title should not be empty',
+  AddTodoError = 'Unable to add a todo',
+  DeleteTodoError = 'Unable to delete a todo',
+  UpdateTodoError = 'Unable to update a todo',
+}
